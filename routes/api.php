d3372fa2c58fce2fa77036451026b3e937c6b7d4
qdung <phanquangdng@gmail.com>
2019-05-12 16:22:28 +0700

    Update. meger master

--- routes/api.php
+++ routes/api.php
@@ -462,5 +462,31 @@ Route::prefix('v1')->group(function () {
             'as' => 'api/v1/auctions/auction'
         ]);
 
+        Route::prefix('cards')->group(function () {
+
+
+            Route::get('{id?}', [
+                'uses' => 'API\CardController@get',
+                'as' => 'api/v1/cards/get'
+            ])->where('id', '[0-9]+');
+
+
+            Route::post('', [
+                'uses' => 'API\CardController@post',
+                'as' => 'api/v1/cards/post'
+            ]);
+
+            Route::patch('{id?}', [
+                'uses' => 'API\CardController@patch',
+                'as' => 'api/v1/cards/patch'
+            ])->where('id', '[0-9]+');
+
+            Route::delete('{id}', [
+                'uses' => 'API\CardController@delete',
+                'as' => 'api/v1/cards/delete'
+            ])->where('id', '[0-9]+');
+
+        });
+
     });
 });
